33ab931caadcad8cb72c734e52ab87c0f71f2174
Çağın Tunç <71410956+cagintunc@users.noreply.github.com>
2024-02-02 13:55:02 +0300

    k2

--- hair_project/hair_project/settings.py
+++ hair_project/hair_project/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 # Turn on WhiteNoise storage backend that takes care of compressing static files
 # and creating unique names for each version so they can safely be cached forever.
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
